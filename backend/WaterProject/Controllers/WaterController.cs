946b45820cb6a8752a8030246745e1ee47a3e0d7
Aaron205 <arno0716@hotmail.com>
2025-05-16 15:58:36 -0500

    Implemented page size limitations

--- backend/WaterProject/Controllers/WaterController.cs
+++ backend/WaterProject/Controllers/WaterController.cs
@@ -13,11 +13,11 @@ namespace WaterProject.Controllers
         public WaterController(WaterDbContext temp) => _waterContext = temp; //constructor builds at startup and can set a waterContext variable to be used between methods.
                                                                              //This can be done in an inline statement like so
         [HttpGet("AllProjects")]//route, is routing
-        public IEnumerable<Project> GetProjects() //goes to the database
+        public IEnumerable<Project> GetProjects(int pageHowMany) //goes to the database,getting how many selected projects we want to display
         {
             //var list = _waterContext.Projects.ToList(); //gets all the projects and returns it to a list
             //var list = _waterContext.Projects.Take(5).ToList(); //gets the first 5 projects 
-            var list = _waterContext.Projects.Skip(5).Take(3).ToList();
+            var list = _waterContext.Projects.Take(pageHowMany).ToList(); //display how many we want
             return list;
         }
 
